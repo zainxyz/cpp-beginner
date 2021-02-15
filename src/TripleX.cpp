21fe893c6e9f33cf531206457bd4acc8c21ae2fd
Zain Abidi <hello@zainabidi.com>
2021-02-15 14:06:09 -0800

    feat: add min/max levels, and update messages

--- src/TripleX.cpp
+++ src/TripleX.cpp
@@ -3,26 +3,29 @@
 using namespace std;
 
 // Declare some global vars.
-string MessageWelcome = "\n\nYou're Lightning McQueen, and want to unlock your full potential!\nYour NOS tank has a combination lock on it and the label underneath reads...\n";
-string MessageWin = "\nFantastic! You've unlocked the NOS tank and are on your way to win the race!";
-string MessageLoose = "\nSorry, but the combination you entered doesn't match the records.\nYou've failed to unlock your NOS tank(s) and might loose the race. Good luck!";
+// string MessageWelcome = "\n\nYou're Lightning McQueen, and want to unlock your full potential!\nYour NOS tank has a combination lock on it and the label underneath reads...\n";
 string MessageAskForInput = "\n\nPlease enter your guess for the 3 digit combination (1, 2, 3) : ";
+string MessageLevelComplete = "\n*** Great! You've unlocked this NOS tank and are on your way to win the race! Keep going! ***";
+string MessageLevelTryAgain = "\n*** Whoops! You've entered the wrong code. Careful as you might loose the race! Try again! ***";
+string MessageWin = "\n*** Fantastic! You've unlocked all the NOS tanks, boosted yourself to the front of the line and won the race! ***";
 
 /**
  * Print the Intro!
  */
-void PrintIntroduction()
+void PrintIntroduction(int Difficulty)
 {
-    cout << MessageWelcome;
+    cout << "\n\n---";
+    cout << "\nYou're Lightning McQueen, and are trying to unlock the level \"" << Difficulty << "\" NOS tank.";
+    cout << "\nYour NOS tank has a combination lock on it and the label underneath reads...\n";
 }
 
 /**
  * Let's play the TripleX game!
  */
-bool PlayGame()
+bool PlayGame(int Difficulty, int MaxLevel)
 {
     // Print welcome message to the terminal.
-    PrintIntroduction();
+    PrintIntroduction(Difficulty);
 
     // Initialize the 3 codes..
     const int CodeA = 1;
@@ -52,12 +55,16 @@ bool PlayGame()
     // Check to see if the guessed sum/product match the actual sum/product.
     if (GuessSum == CodeSum && GuessProduct == CodeProduct)
     {
-        cout << MessageWin;
+        // Only print the level complete message if you're not at max level.
+        if (Difficulty < MaxLevel) 
+        {
+            cout << MessageLevelComplete;
+        }
         // Player has completed the current level.
         return true;
     } else
     {
-        cout << MessageLoose;
+        cout << MessageLevelTryAgain;
         // Player lost the current level;
         return false;
     }
@@ -69,16 +76,32 @@ bool PlayGame()
  */
 int main() 
 {
-    while(true)
+    // Current Level
+    int CurrentLevel = 1;
+    // Max Level
+    int MaxLevel = 5;
+    // GameOver?
+    bool gameOn = true;
+    // Keep the game running while we're not at max level.
+    while(CurrentLevel <= MaxLevel)
     {
         // Declare a bool for level completion.
         // Let's play the game.
-        bool bLevelComplete = PlayGame();
+        bool bLevelComplete = PlayGame(CurrentLevel, MaxLevel);
         // Clear out any errors.
         cin.clear();
         // Discard the input buffer.
         cin.ignore();
+        // Has the user completed the level successfully?
+        if (bLevelComplete)
+        {
+            // Increase the level dificulty.
+            ++CurrentLevel;
+        }
+        
     }
+    // Print game winning message.
+    cout << MessageWin;
     // Return Statement
     return 0;
 }
