da891d5ef57035bc3c164559a7e97939e4510553
Zain Abidi <hello@zainabidi.com>
2021-02-15 10:14:57 -0800

    feat: print hello world to console

--- src/TripleX.cpp
+++ src/TripleX.cpp
@@ -1,4 +1,14 @@
+// # represents a preprocessor directive
+// <iostream> is a header file
+#include <iostream>
+
 int main() 
 {
+    // namespace::myfunction
+    // std  namespace standard
+    // ::   scope operator
+    // cout basic output streams to console
+    // <<   what to output
+    std::cout << "Hello World!";
     return 0;
 }
