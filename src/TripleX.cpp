fd75b9d87fb3ded91045f7fad5db9ec6a9550fce
Zain Abidi <hello@zainabidi.com>
2021-02-15 10:34:05 -0800

    chore: follow unreal engine naming convention

--- src/TripleX.cpp
+++ src/TripleX.cpp
@@ -20,21 +20,25 @@ int main()
     std::cout << "You're a secret agent, breaking into a secure server room!";
     // Add a new line
     std::cout << std::endl;
-    std::cout << "You need to enter the correct codes to continue...";
+    std::cout << "You need to enter the correct codes to continue..." << std::endl;
 
     // Declaration Statements - statements where we "declare" something
     // Declaring 3 number codes
-    const int a = 5;
-    const int b = 6;
-    const int c = 7;
+    const int CodeA = 5;
+    const int CodeB = 6;
+    const int CodeC = 7;
 
-    const int sum = a + b + c;
-    const int product = a * b * c;
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB * CodeC;
 
-    // Print sum and product to the terminal
+    // Print CodeSum and CodeProduct to the terminal
     std::cout << std::endl;
-    std::cout << sum << std::endl;
-    std::cout << product << std::endl;
+    std::cout << "+ There are 3 numbers in the code" << std::endl;
+    std::cout << "+ The codes add-up to: " << CodeSum << std::endl;
+    std::cout << "+ The codes multiply to: " << CodeProduct << std::endl;
+
+    // Store the player's guess
+    int PlayerGuess;
 
     // Return Statement
     return 0;
