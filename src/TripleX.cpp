6e0af3ddff9a2f1dc74c66dff246860d207addf7
Zain Abidi <hello@zainabidi.com>
2021-02-15 12:17:11 -0800

    feat: functions! & cleanup

--- src/TripleX.cpp
+++ src/TripleX.cpp
@@ -1,7 +1,62 @@
-// Preprocessor Directive
-// # represents a preprocessor directive
-// <iostream> is a header file
 #include <iostream>
+#include <string>
+
+// Declare some global vars.
+std::string MessageWelcome = "You're Lightning McQueen, and want to unlock your full potential!\nYour NOS tank has a combination lock on it and the label underneath reads...\n";
+std::string MessageWin = "\nFantastic! You've unlocked the NOS tank and are on your way to win the race!";
+std::string MessageLoose = "\nSorry, but the combination you entered doesn't match the records.\nYou've failed to unlock your NOS tank(s) and might loose the race. Good luck!";
+std::string MessageAskForInput = "\n\nPlease enter your guess for the 3 digit combination (1, 2, 3) : ";
+
+/**
+ * Print the Intro!
+ */
+void PrintIntroduction()
+{
+    std::cout << MessageWelcome;
+}
+
+/**
+ * Let's play the TripleX game!
+ */
+void PlayGame()
+{
+    // Print welcome message to the terminal.
+    PrintIntroduction();
+
+    // Initialize the 3 codes..
+    const int CodeA = 1;
+    const int CodeB = 2;
+    const int CodeC = 3;
+
+    // Evaluate the SUM and PRODUCT of these codes.
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB * CodeC;
+
+    // Print out the CodeSum and CodeProduct hints to the user.
+    std::cout << "\n+ There are 3 numbers in the code.";
+    std::cout << "\n+ The combination add-up to: " << CodeSum;
+    std::cout << "\n+ The combination multipley to: " << CodeProduct;
+
+    // Declare vars for storing player's guesses.
+    int GuessA, GuessB, GuessC;
+
+    // Ask the user for inputting their guesses.
+    std::cout << MessageAskForInput;
+    std::cin >> GuessA >> GuessB >> GuessC;
+    
+    // Calculate the GuessSum and GuessProduct of the inputs..
+    const int GuessSum = GuessA + GuessB + GuessC;
+    const int GuessProduct = GuessA * GuessB * GuessC;
+
+    // Check to see if the guessed sum/product match the actual sum/product.
+    if (GuessSum == CodeSum && GuessProduct == CodeProduct)
+    {
+        std::cout << MessageWin;
+    } else
+    {
+        std::cout << MessageLoose;
+    }
+}
 
 /**
  * TripleX
@@ -9,70 +64,8 @@
  */
 int main() 
 {
-    // namespace::myfunction
-    // std  "standard" namespace
-    // ::   scope operator
-    // cout basic "character output" streams to console
-    // <<   "insertion operator" inserting characters (what's on RHS) to the output stream
-    // std::cout << "Hello World!";
-
-    // Expression Statements - expressions that end with a semicolon
-    // Print welcome messages to the terminal
-    std::cout << "You're Lightning McQueen, and want to unlock your full potential!\n";
-    // std::cout << "You're a secret agent, breaking into a secure server room!";
-    // Add a new line
-    // std::cout << std::endl;
-    // std::cout << "You need to enter the correct codes to continue..." << std::endl;
-    // std::cout << "Your NOS tank has a 3 digit lock on it, and the label underneath reads..." << std::endl;
-    std::cout << "Your NOS tank has a 3 digit lock on it, and the label underneath reads...\n";
-
-    // Declaration Statements - statements where we "declare" something
-    // Declaring 3 number codes
-    const int CodeA = 1;
-    const int CodeB = 2;
-    const int CodeC = 3;
-
-    const int CodeSum = CodeA + CodeB + CodeC;
-    const int CodeProduct = CodeA * CodeB * CodeC;
-
-    // Print CodeSum and CodeProduct to the terminal
-    // std::cout << std::endl;
-    std::cout << "\n+ There are 3 numbers in the code.";
-    std::cout << "\n+ The codes add-up to: " << CodeSum;
-    std::cout << "\n+ The codes multiply to: " << CodeProduct;
-    // std::cout << std::endl;
-
-    // Store the player's guess
-    // Since these are INTs, any non-numeric input will be converted to a '0',
-    // in addition, these non-numeric inputs will halt "cin" from working until reset.
-    int GuessA, GuessB, GuessC;
-
-    std::cout << "\n\nPlease enter your guess for the codes, (1 2 3) : ";
-    // cin         basic "character input" to get user's input
-    // >>          "extraction operator" extract characters from the input stream
-    // PlayerGuess the extracted user's input will be stored in this variable now
-    // If player enters 234 it will be counted as a single number; however,
-    // if the player enters 2 3 4, each space counts as a separate input.
-    std::cin >> GuessA >> GuessB >> GuessC;
-    // std::cout << std::endl;
-
-    const int GuessSum = GuessA + GuessB + GuessC;
-    const int GuessProduct = GuessA * GuessB * GuessC;
-
-    // Print out the uers's guess
-    // std::cout << "You entered: " << GuessA << " " << GuessB << " " << GuessC << std::endl;
-    // std::cout << "Whos sum & product are: " << GuessSum << " " << GuessProduct << std::endl;
-
-    // Check to see if the guess sum / product matches the actual sum / product
-    if (GuessSum == CodeSum && GuessProduct == CodeProduct)
-    {
-        std::cout << "\nFantastic! You've unlocked the NOS tank and are on your way to win the race!";
-    } else
-    {
-        std::cout << "\nSorry, but the code you entered doesn't match the records.\n";
-        std::cout << "You've failed to unlock your NOS tank(s) and might loose the race. Good luck!";
-    }
-
+    // Let's play the game.
+    PlayGame();
     // Return Statement
     return 0;
 }
---
+++ src/old-main-file.cpp
@@ -0,0 +1,78 @@
+// Preprocessor Directive
+// # represents a preprocessor directive
+// <iostream> is a header file
+#include <iostream>
+
+/**
+ * TripleX
+ * Main Function
+ */
+int main() 
+{
+    // namespace::myfunction
+    // std  "standard" namespace
+    // ::   scope operator
+    // cout basic "character output" streams to console
+    // <<   "insertion operator" inserting characters (what's on RHS) to the output stream
+    // std::cout << "Hello World!";
+
+    // Expression Statements - expressions that end with a semicolon
+    // Print welcome messages to the terminal
+    std::cout << "You're Lightning McQueen, and want to unlock your full potential!\n";
+    // std::cout << "You're a secret agent, breaking into a secure server room!";
+    // Add a new line
+    // std::cout << std::endl;
+    // std::cout << "You need to enter the correct codes to continue..." << std::endl;
+    // std::cout << "Your NOS tank has a 3 digit lock on it, and the label underneath reads..." << std::endl;
+    std::cout << "Your NOS tank has a 3 digit lock on it, and the label underneath reads...\n";
+
+    // Declaration Statements - statements where we "declare" something
+    // Declaring 3 number codes
+    const int CodeA = 1;
+    const int CodeB = 2;
+    const int CodeC = 3;
+
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB * CodeC;
+
+    // Print CodeSum and CodeProduct to the terminal
+    // std::cout << std::endl;
+    std::cout << "\n+ There are 3 numbers in the code.";
+    std::cout << "\n+ The codes add-up to: " << CodeSum;
+    std::cout << "\n+ The codes multiply to: " << CodeProduct;
+    // std::cout << std::endl;
+
+    // Store the player's guess
+    // Since these are INTs, any non-numeric input will be converted to a '0',
+    // in addition, these non-numeric inputs will halt "cin" from working until reset.
+    int GuessA, GuessB, GuessC;
+
+    std::cout << "\n\nPlease enter your guess for the codes, (1 2 3) : ";
+    // cin         basic "character input" to get user's input
+    // >>          "extraction operator" extract characters from the input stream
+    // PlayerGuess the extracted user's input will be stored in this variable now
+    // If player enters 234 it will be counted as a single number; however,
+    // if the player enters 2 3 4, each space counts as a separate input.
+    std::cin >> GuessA >> GuessB >> GuessC;
+    // std::cout << std::endl;
+
+    const int GuessSum = GuessA + GuessB + GuessC;
+    const int GuessProduct = GuessA * GuessB * GuessC;
+
+    // Print out the uers's guess
+    // std::cout << "You entered: " << GuessA << " " << GuessB << " " << GuessC << std::endl;
+    // std::cout << "Whos sum & product are: " << GuessSum << " " << GuessProduct << std::endl;
+
+    // Check to see if the guess sum / product matches the actual sum / product
+    if (GuessSum == CodeSum && GuessProduct == CodeProduct)
+    {
+        std::cout << "\nFantastic! You've unlocked the NOS tank and are on your way to win the race!";
+    } else
+    {
+        std::cout << "\nSorry, but the code you entered doesn't match the records.\n";
+        std::cout << "You've failed to unlock your NOS tank(s) and might loose the race. Good luck!";
+    }
+
+    // Return Statement
+    return 0;
+}
