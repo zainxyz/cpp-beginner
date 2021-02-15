5bff77d3cc1c06d003d411f15d4a364ab3662328
Zain Abidi <hello@zainabidi.com>
2021-02-15 12:19:26 -0800

    feat: use the standard namespace

--- src/TripleX.cpp
+++ src/TripleX.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
 // Declare some global vars.
-std::string MessageWelcome = "You're Lightning McQueen, and want to unlock your full potential!\nYour NOS tank has a combination lock on it and the label underneath reads...\n";
-std::string MessageWin = "\nFantastic! You've unlocked the NOS tank and are on your way to win the race!";
-std::string MessageLoose = "\nSorry, but the combination you entered doesn't match the records.\nYou've failed to unlock your NOS tank(s) and might loose the race. Good luck!";
-std::string MessageAskForInput = "\n\nPlease enter your guess for the 3 digit combination (1, 2, 3) : ";
+string MessageWelcome = "You're Lightning McQueen, and want to unlock your full potential!\nYour NOS tank has a combination lock on it and the label underneath reads...\n";
+string MessageWin = "\nFantastic! You've unlocked the NOS tank and are on your way to win the race!";
+string MessageLoose = "\nSorry, but the combination you entered doesn't match the records.\nYou've failed to unlock your NOS tank(s) and might loose the race. Good luck!";
+string MessageAskForInput = "\n\nPlease enter your guess for the 3 digit combination (1, 2, 3) : ";
 
 /**
  * Print the Intro!
  */
 void PrintIntroduction()
 {
-    std::cout << MessageWelcome;
+    cout << MessageWelcome;
 }
 
 /**
@@ -33,16 +34,16 @@ void PlayGame()
     const int CodeProduct = CodeA * CodeB * CodeC;
 
     // Print out the CodeSum and CodeProduct hints to the user.
-    std::cout << "\n+ There are 3 numbers in the code.";
-    std::cout << "\n+ The combination add-up to: " << CodeSum;
-    std::cout << "\n+ The combination multipley to: " << CodeProduct;
+    cout << "\n+ There are 3 numbers in the code.";
+    cout << "\n+ The combination add-up to: " << CodeSum;
+    cout << "\n+ The combination multipley to: " << CodeProduct;
 
     // Declare vars for storing player's guesses.
     int GuessA, GuessB, GuessC;
 
     // Ask the user for inputting their guesses.
-    std::cout << MessageAskForInput;
-    std::cin >> GuessA >> GuessB >> GuessC;
+    cout << MessageAskForInput;
+    cin >> GuessA >> GuessB >> GuessC;
     
     // Calculate the GuessSum and GuessProduct of the inputs..
     const int GuessSum = GuessA + GuessB + GuessC;
@@ -51,10 +52,10 @@ void PlayGame()
     // Check to see if the guessed sum/product match the actual sum/product.
     if (GuessSum == CodeSum && GuessProduct == CodeProduct)
     {
-        std::cout << MessageWin;
+        cout << MessageWin;
     } else
     {
-        std::cout << MessageLoose;
+        cout << MessageLoose;
     }
 }
 
